9a0195467ffd0e4310a10427704c60127714142e
unknown <flavio-barrionuevo09@outlook.com>
2024-05-16 20:34:00 -0300

    adição de uma função para área de uma circunferência

--- areaGeom.py
+++ areaGeom.py
@@ -1,6 +1,14 @@
 # Esse programa é um teste
+
+from math import pi
+
 def area_ret(lado_a, lado_b):
 
     area = lado_a * lado_b
+
+    return area
+
+def area_circ(diametro):
+    area = pi * (diametro ** 2) / 4
 
     return area
